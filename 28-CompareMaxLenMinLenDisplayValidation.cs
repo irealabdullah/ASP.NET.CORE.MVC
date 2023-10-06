844ca31b4505bedbaf60d07004440e41dc8f2747
Abdullah Tariq <irealabdullah@outlook.com>
2023-10-06 15:11:40 +0500

    Commit

--- 28-CompareMaxLenMinLenDisplayValidation.cs
+++ 28-CompareMaxLenMinLenDisplayValidation.cs
@@ -1,1 +1,93 @@
-//[compare]
+// model
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidationAttributes.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please Enter Name")] // Field cannot be null 
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Enter Character between 3-15")]
+        [MinLength(15,ErrorMessage ="Min length = 15")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+       
+
+        [Required(ErrorMessage = "Please Correct Password ;)")]
+        [RegularExpression("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$",ErrorMessage ="Uppercase, Lowercase, symbols")] //find from internet 
+        public string password { get; set; }
+       
+
+
+        [Required(ErrorMessage = "Please Correct Password")]
+        [Compare("password",ErrorMessage ="Password Doesnot Match")]
+        [Display(Name="Enter Confirm Password:")] //ab property ka name show nahi hoo ga 
+        public string Confirmpassword { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Correct number ;)")]
+        [RegularExpression("^((\\+92)|(0092))-{0,1}\\d{3}-{0,1}\\d{7}$|^\\d{11}$|^\\d{4}-\\d{7}$")] 
+        public string PakiNumber{ get; set; }
+
+
+        [Required(ErrorMessage ="Website URL is Must")]
+        [Url]
+        public string WebsiteURL { get; set; }
+
+    }
+}
+
+
+//index.cshtml
+
+@model ValidationAttributes.Models.Student;
+@{
+    ViewData["Title"] = "Home Page";
+}
+
+<div class="text-center">
+    <h1 class="display-4">Welcome</h1>
+    <p>Learn about <a href="https://docs.microsoft.com/aspnet/core">building Web apps with ASP.NET Core</a>.</p>
+</div>
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+<form asp-controller="Home" asp-action="Index" method="post">
+   
+    
+    <input asp-for="Name" class="form-control" placeholder="Enter Name"/>
+    <span asp-validation-for="Name" style="color:red"></span>
+    <br />
+        
+
+    <input asp-for="password" class="form-control" placeholder="Enter Password"/>
+    <span asp-validation-for="password" style="color:red"></span>
+
+    <br />
+    <label asp-for="Confirmpassword"></label> @* ab property ka name show nahi hoo ga *@
+    <input asp-for="Confirmpassword" class="form-control" placeholder="Enter Confirm Password" />
+    <span asp-validation-for="Confirmpassword" style="color:red"></span>
+
+    <br />
+
+    <input asp-for="PakiNumber" class="form-control" placeholder="Enter Contact" />
+    <span asp-validation-for="PakiNumber" style="color:red"></span>
+
+    <br />
+
+    <input asp-for="WebsiteURL" class="form-control" placeholder="Enter Website URL"/>
+    <span asp-validation-for="WebsiteURL" style="color:red"></span>
+
+    <br />
+    <input type="submit" value="Submit" class="btn btn-outline-primary"/>
+
+
+</form>
+        </div>
+    </div>
+
+</div>
---
+++ 29-ValidationTagHelper.cs
@@ -0,0 +1,51 @@
+/*There are two validation tag helper in asp.net core Mvc
+
+validation message Tag Helpers
+exp: asp-validation-for
+
+
+agar mujhe har input tag ke neeche asp-validation-for show nahi rakhwana aur main chahta hoon ke yeh sab alehda show hoo jayeen somewhere else then we will use asp-validation-summary
+*/
+
+<input asp-for="Name" class="form-control" placeholder="Enter Name"/>
+<span asp-validation-for="Name" style="color:red"></span>                  //yeh alehdah likhain 
+
+<input asp-for="password" class="form-control" placeholder="Enter Password"/>
+<span asp-validation-for="password" style="color:red"></span>               //yeh alehdah likhain 
+
+<label asp-for="Confirmpassword"></label> @* ab property ka name show nahi hoo ga *@
+<input asp-for="Confirmpassword" class="form-control" placeholder="Enter Confirm Password" />
+<span asp-validation-for="Confirmpassword" style="color:red"></span>        //yeh alehdah likhain 
+
+
+
+//asp-validation-summary
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+
+            <div asp-validation-summary="All" style="color:red">
+
+            </div>
+<form asp-controller="Home" asp-action="Index" method="post">
+   
+    
+    <input asp-for="Name" class="form-control" placeholder="Enter Name"/>
+    <br />
+                
+    <input asp-for="Email" class="form-control" placeholder="Enter Email"/>
+    <br />
+
+    <input asp-for="Age" class="form-control" placeholder="Enter Age"/>
+    <br />
+               
+
+    <input type="submit" value="Submit" class="btn btn-outline-primary"/>
+
+
+</form>
+        </div>
+    </div>
+
+</div>
